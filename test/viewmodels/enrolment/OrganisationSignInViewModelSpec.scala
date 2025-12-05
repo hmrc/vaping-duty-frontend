f5f6d16d6583708ca2865ea40de663f842e7d17d
asadhmrc <232776629+asadhmrc@users.noreply.github.com>
2025-12-05 12:21:40 +0000

    Add spec for OrgSignIn view model

---
+++ test/viewmodels/enrolment/OrganisationSignInViewModelSpec.scala
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package viewmodels.enrolment
+
+import base.UnitSpec
+import config.FrontendAppConfig
+
+import org.mockito.Mockito.*
+
+
+class OrganisationSignInViewModelSpec extends UnitSpec {
+
+  "OrganisationSignInViewModel" - {
+
+    val config = mock[FrontendAppConfig]
+    when(config.organisationAcctGuidanceUrl).thenReturn("https://www.tax.service.gov.uk/account")
+    when(config.organisationSignInUrl).thenReturn("https://www.tax.service.gov.uk/account")
+
+    val vm = OrganisationSignInViewModel(config)
+
+    "returns correct URL for orgSignIn" in {
+        vm.getOrgSignInURL mustBe config.organisationSignInUrl
+    }
+
+    "returns correct URL for orgAcctGuidance" in {
+      vm.getGuidanceURL mustBe config.organisationAcctGuidanceUrl
+    }
+
+  }
+}
