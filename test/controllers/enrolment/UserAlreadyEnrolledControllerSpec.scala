bf3238110b881cafef7eb5d0b6cc2aec449c504f
asadhmrc <232776629+asadhmrc@users.noreply.github.com>
2025-12-17 17:20:24 +0000

    Add 'UserAlreadyEnrolled' controller and spec

---
+++ test/controllers/enrolment/UserAlreadyEnrolledControllerSpec.scala
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.enrolment
+
+import base.SpecBase
+import play.api.test.FakeRequest
+import play.api.test.Helpers.*
+import viewmodels.enrolment.UserAlreadyEnrolledViewModel
+import views.html.enrolment.UserAlreadyEnrolledView
+
+class UserAlreadyEnrolledControllerSpec extends SpecBase {
+
+  "UserAlreadyEnrolled Controller" - {
+
+    "must return OK and the correct view for a GET" in {
+
+      val application = applicationBuilder().build()
+
+      running(application) {
+        val request = FakeRequest(GET, routes.UserAlreadyEnrolledController.onPageLoad().url)
+
+        val result = route(application, request).value
+
+        val view = application.injector.instanceOf[UserAlreadyEnrolledView]
+        val vm = application.injector.instanceOf[UserAlreadyEnrolledViewModel]
+        
+        status(result) mustEqual OK
+        contentAsString(result) mustEqual view(vm)(request, messages(application)).toString
+      }
+    }
+  }
+}
