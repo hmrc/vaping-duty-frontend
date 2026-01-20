4ad9e9c0a79e34943909c18ad50d7afd079e765e
ghousedev <49005870+ghousedev@users.noreply.github.com>
2026-01-20 17:56:45 +0000

    VPD-1188: Content for 'continue postal preference' page

--- app/controllers/ContinuePostalPreferenceController.scala
+++ app/controllers/ContinuePostalPreferenceController.scala
@@ -32,7 +32,7 @@ class ContinuePostalPreferenceController @Inject()(
                                        view: ContinuePostalPreferenceView
                                      ) extends FrontendBaseController with I18nSupport {
 
-  def onPageLoad: Action[AnyContent] = (identify andThen getData andThen requireData) {
+  def onPageLoad: Action[AnyContent] = (identify) {
     implicit request =>
       Ok(view())
   }
