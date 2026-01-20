0c8126ca12fcfdc1bb5f2533bf9ba8cfe3971a04
ghousedev <49005870+ghousedev@users.noreply.github.com>
2026-01-20 17:21:41 +0000

    VPD-1188: Content for 'email confirmation' page

--- app/controllers/EmailConfirmationController.scala
+++ app/controllers/EmailConfirmationController.scala
@@ -32,7 +32,7 @@ class EmailConfirmationController @Inject()(
                                        view: EmailConfirmationView
                                      ) extends FrontendBaseController with I18nSupport {
 
-  def onPageLoad: Action[AnyContent] = (identify andThen getData andThen requireData) {
+  def onPageLoad: Action[AnyContent] = (identify) {
     implicit request =>
       Ok(view())
   }
