b3d3c4599c6f6aa63b612530f10e2303970142f8
Rob Gresham <49005870+ghousedev@users.noreply.github.com>
2026-03-03 15:50:13 +0000

    VPD-1418: Add explicit sign out redirect for session timeout (#55)
    * VPD-1418: Add explicit sign out redirect for session timeout
    
    * VPD-1418: Update specs

--- app/controllers/auth/AuthController.scala
+++ app/controllers/auth/AuthController.scala
@@ -17,40 +17,30 @@
 package controllers.auth
 
 import config.FrontendAppConfig
-import controllers.actions.CheckSignedInAction
 import play.api.i18n.I18nSupport
 import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
-import services.UserAnswersService
 import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
 
 import javax.inject.Inject
-import scala.concurrent.ExecutionContext
 
 
 class AuthController @Inject()(
                                 val controllerComponents: MessagesControllerComponents,
-                                config: FrontendAppConfig,
-                                sessionRepository: UserAnswersService,
-                                ifSignedIn: CheckSignedInAction
-                              )(implicit ec: ExecutionContext) extends FrontendBaseController with I18nSupport {
+                                config: FrontendAppConfig
+                              ) extends FrontendBaseController with I18nSupport {
 
-  def signOut(): Action[AnyContent] = ifSignedIn.async {
-    implicit request =>
-      sessionRepository
-        .clear(request.internalId)
-        .map {
-          _ =>
-            Redirect(config.signOutUrl, Map("continue" -> Seq(config.exitSurveyUrl)))
-      }
+  /**
+   * When generated the below methods had the functionality to clear the user answers stored in this repository when a user signs out.
+   * As we have moved our user answers to the backend some authentication was required to clear.  Our timeout lines up with the auth session
+   * of 15 minutes which could cause the use of these methods when timed out to return as unauthorised, causing the wrong page to be displayed.
+   * The TTL currently used will clear the answers at 15 minutes, so an explicit call to clear them is not needed.
+   */
+
+  def signOut(): Action[AnyContent] = Action {
+    Redirect(config.signOutUrl, Map("continue" -> Seq(config.exitSurveyUrl)))
   }
 
-  def signOutNoSurvey(): Action[AnyContent] = ifSignedIn.async {
-    implicit request =>
-    sessionRepository
-      .clear(request.internalId)
-      .map {
-        _ =>
-        Redirect(config.signOutUrl, Map("continue" -> Seq(routes.SignedOutController.onPageLoad().url)))
-      }
+  def signOutNoSurvey(): Action[AnyContent] = Action {
+    Redirect(routes.SignedOutController.onPageLoad().url)
   }
 }
