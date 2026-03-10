b642399b5f1698c99f27dbd2afe9fb970d2ea0e5
Asad Humayun <232776629+asadhmrc@users.noreply.github.com>
2026-03-10 13:04:16 +0000

    VPD-1424: Add ability for multiple illegalChars and multiple errors

--- it/test/MessagesISpec.scala
+++ it/test/MessagesISpec.scala
@@ -16,52 +16,59 @@
 
 import org.scalactic.source.Position
 import org.scalatest.freespec.AnyFreeSpec
+import org.scalatest.matchers.must.Matchers
 
-import java.nio.file.Paths
+import java.nio.file.{Path, Paths}
 import scala.io.Source.fromFile as readFile
-import java.nio.file.Path
 
-class MessagesISpec extends AnyFreeSpec {
-  private val illegalChar: String = "'"
+class MessagesISpec extends AnyFreeSpec with Matchers {
+  private val illegalChars = List("'")
 
   private def readFileLines(path: String) = {
-    val source = readFile(path)
+    val source   = readFile(path)
+    val lines    = source.getLines.toList
+    val linesMap = lines.zipWithIndex.map { case (line, idx) => idx -> line }.toMap
 
-    try source.getLines mkString "\n" finally source.close()
+    source.close()
+
+    linesMap
+  }
+
+  private def assertNoIllegalCharsPresent(lines: Map[Int, String]): Unit = {
+    lines foreach { case (idx, line) =>
+      illegalChars foreach { illegal =>
+        line must not include(illegal)
+      }
+    }
   }
 
   "messages files " - {
-    "must not contain a non-smart punctuation apostrophe in conf/messages.en" in {
+    s"must not contain any illegal characters (${illegalChars.mkString(",")}) in conf/messages.en" in {
       val messagesFilePath = resolveMessageFile("messages.en")
-      val lines = readFileLines(messagesFilePath)
-
-      if (lines.contains(illegalChar)) {
-        throw new Error(s"messages.en file contains illegal character \"$illegalChar\"; the smart punctuation variant should be used instead.")
-      }
+      val lines            = readFileLines(messagesFilePath)
+      
+      assertNoIllegalCharsPresent(lines)
     }
 
-    "must not contain a non-smart punctuation apostrophe in conf/messages.cy" in {
-      val messagesFilePath: String = resolveMessageFile("messages.cy")
-      val lines = readFileLines(messagesFilePath)
-
-      if (lines.contains(illegalChar)) {
-        throw new Error(s"messages.cy file contains illegal character \"$illegalChar\"; the smart punctuation variant should be used instead.")
-      }
+    s"must not contain any illegal characters (${illegalChars.mkString(",")}) in conf/messages.cy" in {
+      val messagesFilePath = resolveMessageFile("messages.cy")
+      val lines            = readFileLines(messagesFilePath)
+      
+      assertNoIllegalCharsPresent(lines)
     }
   }
 
   /** This series of methods resolve the path of the messages files.
-   * @note the file path when executing it/test through sbt is different
-   *       from that which is present when executing it/test through IDEs.
-   *
-   *       This method attempts to resolve this by detecting the user dir
-   *       of the executing process.
-   */
+    * @note
+    *   the file path when executing it/test through sbt is different from that which is present when executing it/test through IDEs.
+    *
+    * This method attempts to resolve this by detecting the user dir of the executing process.
+    */
 
-    def moveToConfFile(path: Path, messageFileName: String) = path
-                              .resolve("conf")
-                              .resolve(messageFileName)
-                              .toString
+  def moveToConfFile(path: Path, messageFileName: String) = path
+    .resolve("conf")
+    .resolve(messageFileName)
+    .toString
 
   private def resolveMessageFile(messageFileName: String): String = {
     val userDir = Paths.get(System.getProperty("user.dir"))
