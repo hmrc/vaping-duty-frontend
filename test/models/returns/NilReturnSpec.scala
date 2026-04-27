ff88d5094e76cc657d1e2f2a9bafa470f1bf62a9
craigemmerson <21197340+craigemmerson@users.noreply.github.com>
2026-04-27 16:55:41 +0100

    [VPD-1393] added model specs

---
+++ test/models/returns/NilReturnSpec.scala
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2024 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package models.returns
+
+import base.SpecBase
+import play.api.libs.json.Json
+
+class NilReturnSpec extends SpecBase {
+  val nilReturn: NilReturn =
+    NilReturn("nil")
+
+  "NilReturn" - {
+    val json = """{"vapingProductsProduced":"nil"}"""
+
+    "must serialise to json" in {
+      Json.toJson(nilReturn).toString() mustBe json
+    }
+
+    "must deserialise from json" in {
+      Json.parse(json).as[NilReturn] mustBe nilReturn
+    }
+  }
+}
