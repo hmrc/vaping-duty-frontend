c5a1bf968f41961b3374a63e8c120e1914ab74b4
ghousedev <49005870+ghousedev@users.noreply.github.com>
2026-04-29 09:12:47 +0100

    VPD-1489: Refactor added files

--- app/viewmodels/returns/ViewMultipleReturnsViewModel.scala
+++ app/viewmodels/returns/ViewMultipleReturnsViewModel.scala
@@ -16,31 +16,24 @@
 
 package viewmodels.returns
 
+import models.returns.multiple.{CompletedReturnRow, OutstandingReturnRow}
 import models.returns.{ObligationDetails, ObligationsResponse}
+import views.html.components.*
 import play.api.i18n.Messages
+import uk.gov.hmrc.govukfrontend.views.Aliases.{HtmlContent, TableRow, Tag}
+import uk.gov.hmrc.govukfrontend.views.html.components.GovukTag
+import uk.gov.hmrc.govukfrontend.views.viewmodels.content.Text
 
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 import java.util.Locale
 
-case class OutstandingReturnRow(
-  monthDisplay: String,
-  status: String,
-  statusClass: String,
-  submitLink: String
-)
+case class ViewMultipleReturnsViewModel(
+                                     outstandingReturns: Seq[Seq[TableRow]],
+                                     completedReturns: Seq[Seq[TableRow]]
+                                   )
 
-case class CompletedReturnRow(
-  monthDisplay: String,
-  viewLink: String
-)
-
-case class ReturnsOverviewViewModel(
-  outstandingReturns: Seq[OutstandingReturnRow],
-  completedReturns: Seq[CompletedReturnRow]
-)
-
-object ReturnsOverviewViewModel {
+object ViewMultipleReturnsViewModel {
 
   private val STATUS_OPEN = "O"
   private val STATUS_FULFILLED = "F"
@@ -48,34 +41,65 @@ object ReturnsOverviewViewModel {
   private val TAG_CLASS_BLUE = "govuk-tag--blue"
   private val TAG_CLASS_RED = "govuk-tag--red"
 
-  def apply(obligationsResponse: ObligationsResponse)(implicit messages: Messages): ReturnsOverviewViewModel = {
-    
+  def apply(obligationsResponse: ObligationsResponse)(implicit messages: Messages): ViewMultipleReturnsViewModel = {
+
+    val govukTag = GovukTag()
+    val link = Link()
+
     val outstandingObligations = obligationsResponse.obligation
       .filter(_.obligationDetails.openOrFulfilledStatus == STATUS_OPEN)
       .map(item => createOutstandingRow(item.obligationDetails))
-    
+
     val completedObligations = obligationsResponse.obligation
       .filter(_.obligationDetails.openOrFulfilledStatus == STATUS_FULFILLED)
       .map(item => createCompletedRow(item.obligationDetails))
+
+    val outstandingObligationsRows = outstandingObligations.map { row =>
+      Seq(
+        TableRow(
+          content = Text(row.monthDisplay)
+        ),
+        TableRow(
+          content = HtmlContent(govukTag(Tag(
+            content = Text(row.status),
+            classes = row.statusClass
+          )))
+        ),
+        TableRow(
+          content = HtmlContent(link(id = "submit-link", href = row.submitLink, text = messages("returns.overview.outstanding.submitReturn")))
+        )
+      )
+    }
     
-    ReturnsOverviewViewModel(
-      outstandingReturns = outstandingObligations,
-      completedReturns = completedObligations
+    val completedObligationsRows = completedObligations.map { row =>
+      Seq(
+        TableRow(
+          content = Text(row.monthDisplay)
+        ),
+        TableRow(
+          content = HtmlContent(link(id = "view-link", href = row.viewLink, text = messages("returns.overview.completed.viewReturn")))
+        )
+      )
+    }
+
+    ViewMultipleReturnsViewModel(
+      outstandingReturns = outstandingObligationsRows,
+      completedReturns = completedObligationsRows
     )
   }
 
   private def createOutstandingRow(details: ObligationDetails)(implicit messages: Messages): OutstandingReturnRow = {
     val monthDisplay = formatMonthYear(details.iCFromDate)
     val isOverdue = details.iCDueDate.isBefore(LocalDate.now())
-    
+
     val status = if (isOverdue) {
       messages("returns.overview.outstanding.status.overdue")
     } else {
       messages("returns.overview.outstanding.status.due")
     }
-    
+
     val statusClass = if (isOverdue) TAG_CLASS_RED else TAG_CLASS_BLUE
-    
+
     OutstandingReturnRow(
       monthDisplay = monthDisplay,
       status = status,
