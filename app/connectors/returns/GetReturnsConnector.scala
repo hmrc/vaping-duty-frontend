68c9255287806699fd4756dabb1efd18054341cc
Asad Humayun <105254706+AsadHumayun@users.noreply.github.com>
2026-05-07 17:58:13 +0100

    VPD-1528: Backports GetConnector

---
+++ app/connectors/returns/GetReturnsConnector.scala
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package connectors.returns
+
+import play.api.Logging
+import play.api.libs.json.Json
+import play.api.libs.ws.JsonBodyWritables.*
+import uk.gov.hmrc.http.*
+import uk.gov.hmrc.http.client.HttpClientV2
+import config.FrontendAppConfig
+import models.identifiers.VpdId
+import models.returns.view.ReturnDisplayResponse
+
+import java.time.{Clock, Instant}
+import javax.inject.Inject
+import scala.concurrent.{ExecutionContext, Future}
+import scala.util.{Failure, Success, Try}
+
+class GetReturnsConnector @Inject()(clock: Clock)(
+                                      config: FrontendAppConfig,
+                                      implicit val httpClient: HttpClientV2
+)(implicit ec: ExecutionContext)
+    extends HttpReadsInstances
+    with Logging {
+
+  private val parsingError = "Parsing failed for VPD return get response"
+
+  def getReturn(periodKey: String, vpdId: VpdId)
+                  (implicit hc: HeaderCarrier): Future[ReturnDisplayResponse] =
+    httpClient
+      .get(url"${config.getReturnUrl(vpdId, periodKey)}")
+      .execute[Either[UpstreamErrorResponse, HttpResponse]]
+      .recoverWith { case e: Exception =>
+        logger.warn(s"Exception while getting return: ${e.getMessage}")
+        Future.failed(InternalServerException("Failed to get return"))
+      }
+      .flatMap(response => getReturnParser(response))
+
+  private def getReturnParser(response: Either[UpstreamErrorResponse, HttpResponse]): Future[ReturnDisplayResponse] = {
+    response match {
+      case Right(response) =>
+        Try {
+          response.json.as[ReturnDisplayResponse]
+        } match {
+          case Success(returnsData) => Future.successful(returnsData)
+          case Failure(_) =>
+            logger.warn(parsingError)
+            Future.failed(InternalServerException(parsingError))
+        }
+      case Left(error) =>
+        logger.warn(s"Unexpected response from VPD return get API. Status: ${error.statusCode}")
+        Future.failed(InternalServerException("Failed to get VPD return"))
+    }
+  }
+}
