34e1eb5aabf295bcc2370d781c65058d825c5bc8
Asad Humayun <105254706+AsadHumayun@users.noreply.github.com>
2026-05-07 17:57:39 +0100

    VPD-1528: viewmodel, controller, view

---
+++ app/controllers/returns/view/ViewIndividualReturnController.scala
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.returns.view
+
+import controllers.actions.returns.*
+import controllers.actions.ApprovedVapingManufacturerAuthAction
+import play.api.i18n.{I18nSupport, MessagesApi}
+import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
+import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
+import connectors.returns.GetReturnsConnector
+import viewmodels.returns.view.ViewIndividualReturnViewModel
+import views.html.returns.view.ViewIndividualReturnView
+
+import javax.inject.Inject
+import scala.concurrent.ExecutionContext
+
+class ViewIndividualReturnController @Inject()(
+                                       override val messagesApi: MessagesApi,
+                                       identify: ApprovedVapingManufacturerAuthAction,
+                                       connector: GetReturnsConnector,
+                                       val controllerComponents: MessagesControllerComponents,
+                                       view: ViewIndividualReturnView,
+                                       returnsEnabled: ReturnsEnabledAction
+                                     )(using ExecutionContext) extends FrontendBaseController with I18nSupport {
+
+  def onPageLoad(year: String, month: String): Action[AnyContent] = (identify andThen returnsEnabled).async {
+    implicit request =>
+      // "/view-returns/2027/January"
+      val periodKey = s"${year.takeRight(2)}${utils.PeriodKeys.toEtmpMonthString(month)}"
+      println(s">>>>>>>>>> inside the request")
+      connector.getReturn(periodKey, vpdId = request.enrolmentVpdId)
+        .map { returnData =>
+          Ok(view(ViewIndividualReturnViewModel(returnData)))
+        }
+  }
+}
