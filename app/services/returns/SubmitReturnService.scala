fc060f165ddbdc39fb368346d70baca882e4572c
ian-hmrc <137287384+ian-hmrc@users.noreply.github.com>
2026-07-07 11:49:46 +0100

    VPD-1775 initial refactoring (#121)
    * VPD-1775 Tidy up a little by importing VpdId directly
    
    * VPD-1775 Prepare to extract a function to make the VapingProductsProduced
    
    * VPD-1775 Extract the buildVapingProductsProduced method
    
    * VPD-1775 Look up the duty rate outside the spoilt
    
    This is a step to removing the need for the Future to look it up for each spoilt period
    
    * VPD-1775 Remove the internal lookup of the duty rate
    
    This simplifies the code a lot and removes the need for the Future in the calculation.
    
    * VPD-1775 Start to add simple validation to the spoilt case
    
    Detect valid data (Yes & spoilt data, No & no spoilt data) and process correctly. Invalid data (all other cases) are returned as a None for now. This will need correctly handling at the higher level in a future commit.
    
    * VPD-1775 Extract a method to build the SpoiltProductItem
    
    * VPD-1775 Remove unnecessary stubbing from test
    
    * VPD-1775 Make the spoilt test scenario more realistic
    
    * VPD-1775 Extract a helper to stub obligations
    
    * VPD-1775 Use the real TotalDutyDueCalculationService rather than a mock
    
    This saves a lot of mocking in the tests and doesn't change the outcome. This tells us that our tests are not great, and that we are probably testing at the wrong level.
    
    * VPD-1775 Expose buildSubmission() method which is a pure function, and test it directly
    
    * VPD-1775 Remove the dependency of buildSubmission() on DutyRateService
    
    * VPD-1775 Split BuildReturnSubmissionService from SubmitReturnService
    
    This focuses the business logic in BuildReturnSubmissionService which can be tested more directly, and leaves the job of fetching data and dealing with failures to SubmitReturnService.
    
    * VPD-1775 Correctly build submissions for 'no' cases
    
    This requires setting up more authentic user answers, so start with a nil return and set the fields required for the scenario being tested.
    
    * VPD-1775 Fix issue with test data not having duty rate going back far enough

--- app/services/returns/SubmitReturnService.scala
+++ app/services/returns/SubmitReturnService.scala
@@ -18,13 +18,13 @@ package services.returns
 
 import config.FrontendAppConfig
 import connectors.returns.SubmitReturnConnector
-import models.identifiers.PeriodKey
+import models.identifiers.{PeriodKey, VpdId}
 import models.obligations.ObligationDetails
 import models.requests.returns.ReturnsDataRequest
 import models.returns.*
 import models.returns.submit.{ReturnCreateRequest, ReturnSubmittedResponse}
 import models.returns.view.{OtherOptions, OverDeclaration, SpoiltProduct, SpoiltProductItem, UnderDeclaration}
-import pages.returns.{DeclarationPage, DeclareDutyPage, DeclareDutySuspensePage, EnterDutyAmountPage, EnterDutySuspensePage, SpoiltVolumeByPeriodPage}
+import pages.returns.{DeclarationPage, DeclareDutyPage, DeclareDutySuspensePage, DeclareSpoiltProductsPage, EnterDutyAmountPage, EnterDutySuspensePage, SpoiltVolumeByPeriodPage}
 import services.contactPreference.AuditService
 import uk.gov.hmrc.http.HeaderCarrier
 import uk.gov.hmrc.play.http.HeaderCarrierConverter
@@ -36,14 +36,9 @@ class SubmitReturnService @Inject()(
   submitReturnConnector: SubmitReturnConnector,
   dutyRateService: DutyRateService,
   obligationService: ObligationService,
-  totalDutyDueCalculationService: TotalDutyDueCalculationService,
+  buildReturnSubmissionService: BuildReturnSubmissionService,
   auditService: AuditService,
-  config: FrontendAppConfig
 )(using ExecutionContext) {
-
-  private val ZERO_VALUE = BigDecimal("0")
-  private val FLAG_NOT_FILLED = "0"
-  private val FLAG_FILLED = "1"
 
   def submit(ua: ReturnsUserAnswers)(implicit request: ReturnsDataRequest[?]): Future[ReturnSubmittedResponse] = {
 
@@ -51,151 +46,19 @@ class SubmitReturnService @Inject()(
 
     for {
       obligations <- obligationService.getObligationsDirectly(request.enrolmentVpdId)
+      periodKeyToDutyRateInPencePerMl = dutyRateService.getDutyRatesInPencePerMlForPeriodKeys(obligations)
       obligationOpt = obligations.find(_.periodKey == request.periodKey.toString)
       obligation <- obligationOpt match {
         case Some(obl) => Future.successful(obl)
         case None => Future.failed(new IllegalStateException(s"No obligation found for period key: ${ua.periodKey}"))
       }
-      submission <- buildSubmission(ua, obligation, request.enrolmentVpdId)
+      submission = buildReturnSubmissionService.buildSubmission(ua, obligation, request.enrolmentVpdId, periodKeyToDutyRateInPencePerMl)
       result <- submitReturnConnector.submitReturn(submission, request.enrolmentVpdId)
     } yield {
       auditService.auditReturnSubmitted(
         SubmitReturnAuditEvent.buildExplicitAuditEvent(submission, result, request.identifiers, obligations))
  
       result
-    }
-  }
-
-  private def buildSubmission(ua: ReturnsUserAnswers, obligation: ObligationDetails, vpdId: models.identifiers.VpdId)(using HeaderCarrier): Future[ReturnCreateRequest] = {
-
-    val dutyDeclared = ua.get(DeclareDutyPage).getOrElse(false)
-    val liquidInMl = ua.get(EnterDutyAmountPage).getOrElse(ZERO_VALUE)
-
-    val periodKey = PeriodKey(ua.periodKey)
-
-    val dutyRateInPencePerMl: Int = dutyRateService.getRateForDate(obligation.iCFromDate)
-
-    val liquidInLitres = ConvertToLitres(liquidInMl).toLitres
-
-    val dutyDue = (liquidInMl * (BigDecimal(dutyRateInPencePerMl) / 100)).setScale(2, BigDecimal.RoundingMode.DOWN)
-
-    val dutyRateInPoundsPer10Ml = (BigDecimal(dutyRateInPencePerMl) / 100) * 10
-    
-    val vapingProductsProduced = if (dutyDeclared) {
-      VapingProductsProduced(vapingProdManufactured = FLAG_FILLED, returns = Seq(
-        RegularReturn(
-          taxType = config.taxType,
-          dutyRate = dutyRateInPoundsPer10Ml,
-          amountProducedLiquid = liquidInLitres,
-          dutyDue = dutyDue
-        )))
-    } else {
-      VapingProductsProduced(vapingProdManufactured = FLAG_NOT_FILLED, returns = Seq())
-    }
-
-    val totalDutyDueVapingProducts = if (dutyDeclared) dutyDue else ZERO_VALUE
-
-    val underDeclaration = buildUnderDeclaration()
-    val overDeclaration = buildOverDeclaration()
-    val otherOptions = buildOtherOptions(ua)
-
-    buildSpoiltProduct(ua, vpdId).map { spoiltProduct =>
-      val totalDutyDue = totalDutyDueCalculationService.calculate(
-        totalDutyDueVapingProducts,
-        underDeclaration,
-        overDeclaration,
-        spoiltProduct
-      )
-
-      val declaration = ua.get(DeclarationPage).getOrElse(
-        // scalafix:off DisableSyntax.throw
-        throw new IllegalStateException("Declaration details are required for submission")
-      )
-
-      ReturnCreateRequest(
-        periodKey = periodKey.toString,
-        vapingProductsProduced = vapingProductsProduced,
-        underDeclaration = underDeclaration,
-        overDeclaration = overDeclaration,
-        spoiltProduct = spoiltProduct,
-        totalDutyDue = totalDutyDue,
-        otherOptions = otherOptions,
-        declaration = declaration
-      )
-    }
-  }
-
-  private def buildUnderDeclaration(): Option[UnderDeclaration] =
-    Some(UnderDeclaration(
-      underDeclFilled = FLAG_NOT_FILLED,
-      reasonForUnderDecl = None,
-      underDeclarationProducts = None
-    ))
-
-  private def buildOverDeclaration(): Option[OverDeclaration] =
-    Some(OverDeclaration(
-      overDeclFilled = FLAG_NOT_FILLED,
-      reasonForOverDecl = None,
-      overDeclarationProducts = None
-    ))
-
-  private def buildSpoiltProduct(ua: ReturnsUserAnswers, vpdId: models.identifiers.VpdId)(using HeaderCarrier): Future[Option[SpoiltProduct]] = {
-    val spoiltVolumes = ua.get(SpoiltVolumeByPeriodPage)
-    
-    spoiltVolumes match {
-      case Some(volumes) if volumes.nonEmpty =>
-        val spoiltProductsFuture = Future.traverse(volumes) { spoiltVolume =>
-          dutyRateService.getDutyRate(vpdId, spoiltVolume.periodKey).map { dutyRateForPeriod =>
-            val dutyRateInPencePerMl = (dutyRateForPeriod * 100).toInt
-            val dutyRateInPoundsPer10Ml = dutyRateForPeriod * 10
-            val volumeInMl = BigDecimal(spoiltVolume.volume)
-            val volumeInLitres = ConvertToLitres(volumeInMl).toLitres
-            val dutyDue = (volumeInMl * (BigDecimal(dutyRateInPencePerMl) / 100)).setScale(2, BigDecimal.RoundingMode.DOWN)
-            
-            SpoiltProductItem(
-              returnPeriodAffected = spoiltVolume.periodKey.toString,
-              taxType = config.taxType,
-              dutyRate = dutyRateInPoundsPer10Ml,
-              amountSpoilt = volumeInLitres,
-              dutyDue = dutyDue
-            )
-          }
-        }
-        
-        spoiltProductsFuture.map { spoiltProducts =>
-          Some(SpoiltProduct(
-            spoiltProductFilled = FLAG_FILLED,
-            spoiltProducts = Some(spoiltProducts)
-          ))
-        }
-      case _ =>
-        Future.successful(Some(SpoiltProduct(
-          spoiltProductFilled = FLAG_NOT_FILLED,
-          spoiltProducts = None
-        )))
-    }
-  }
-
-  private def buildOtherOptions(ua: ReturnsUserAnswers): Option[OtherOptions] = {
-    val dutySuspenseDeclared = ua.get(DeclareDutySuspensePage).getOrElse(false)
-    val dutySuspenseVolumes = ua.get(EnterDutySuspensePage)
-    
-    (dutySuspenseDeclared, dutySuspenseVolumes) match {
-      case (true, Some(volumes)) =>
-        val volumeReceivedInMl = BigDecimal(volumes.volumeReceived)
-        val volumeMovedInMl = BigDecimal(volumes.volumeMoved)
-        
-        Some(OtherOptions(
-          vapingProductUnderDutySuspense = FLAG_FILLED,
-          volumeMovedFromDutySuspense = Some(ConvertToLitres(volumeReceivedInMl).toLitres),
-          volumeMovedToDutySuspense = Some(ConvertToLitres(volumeMovedInMl).toLitres)
-        ))
-      case _ =>
-        Some(OtherOptions(
-          vapingProductUnderDutySuspense = FLAG_NOT_FILLED,
-          volumeMovedFromDutySuspense = None,
-          volumeMovedToDutySuspense = None
-        ))
     }
   }
 }
---
+++ test/services/returns/BuildReturnSubmissionServiceSpec.scala
@@ -0,0 +1,232 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package services.returns
+
+import base.SpecBase
+import builders.ObligationsBuilders
+import config.FrontendAppConfig
+import connectors.returns.SubmitReturnConnector
+import models.identifiers.{PeriodKey, VpdId}
+import models.obligations.ObligationDetails
+import models.requests.returns.ReturnsDataRequest
+import models.returns.*
+import models.returns.submit.{ReturnCreateRequest, ReturnSubmittedResponse}
+import org.mockito.ArgumentMatchers.{any, eq as eqTo}
+import org.mockito.Mockito.{never, reset, verify, when}
+import org.scalatest.BeforeAndAfterEach
+import org.scalatestplus.mockito.MockitoSugar
+import pages.returns.*
+import play.api.libs.json.JsObject
+import play.api.test.FakeRequest
+import services.contactPreference.AuditService
+
+import java.time.{Instant, LocalDate}
+import scala.concurrent.Future
+
+class BuildReturnSubmissionServiceSpec extends SpecBase with MockitoSugar with BeforeAndAfterEach with ObligationsBuilders {
+
+  private val mockTotalDutyDueCalculationService = TotalDutyDueCalculationService()
+  private val mockConfig = mock[FrontendAppConfig]
+
+  private val service = new BuildReturnSubmissionService(
+    mockTotalDutyDueCalculationService,
+    mockConfig
+  )
+
+  private val vpdId = VpdId("GBWK1234567WK")
+  private val periodKey = PeriodKey("24KA")
+  private val taxType = "641"
+
+  private val obligation = ObligationDetails(
+    openOrFulfilledStatus = "O",
+    iCFromDate = LocalDate.of(2024, 1, 1),
+    iCToDate = LocalDate.of(2024, 1, 31),
+    iCDateReceived = None,
+    iCDueDate = LocalDate.of(2024, 2, 28),
+    periodKey = periodKey.value
+  )
+
+  private val declaration = DeclarationDetails(
+    fullName = "John Smith",
+    capacityInWhichSigned = "Director",
+    signeesEmailAddress = "john.smith@example.com"
+  )
+
+  override def beforeEach(): Unit = {
+    super.beforeEach()
+    when(mockConfig.taxType).thenReturn(taxType)
+  }
+
+  private val yes = "1"
+  private val no = "0"
+
+  "SubmitReturnService" - {
+
+    "submit must" - {
+
+      "successfully build a return submission with duty declared" in {
+        val userAnswers = nilReturnUserAnswers(vpdId, periodKey)
+          .set(DeclareDutyPage, true).success.value
+          .set(EnterDutyAmountPage, BigDecimal("1000")).success.value
+          .set(DeclarationPage, declaration).success.value
+
+        val returnCreateRequest = service.buildSubmission(userAnswers, obligation, vpdId, Map(PeriodKey(obligation.periodKey) -> 1050))
+        
+        returnCreateRequest.vapingProductsProduced.vapingProdManufactured mustBe yes
+        returnCreateRequest.vapingProductsProduced.returns.size mustBe 1
+        returnCreateRequest.vapingProductsProduced.returns.head mustBe RegularReturn("641", 105.0, 1, 10500)
+
+        returnCreateRequest.spoiltProduct.get.spoiltProductFilled mustBe no
+        returnCreateRequest.underDeclaration.get.underDeclFilled mustBe no
+        returnCreateRequest.overDeclaration.get.overDeclFilled mustBe no
+        
+        returnCreateRequest.otherOptions.get.vapingProductUnderDutySuspense mustBe no
+
+        returnCreateRequest.periodKey mustBe periodKey.value
+        returnCreateRequest.declaration.fullName mustBe "John Smith"
+        returnCreateRequest.declaration.capacityInWhichSigned mustBe "Director"
+        returnCreateRequest.declaration.signeesEmailAddress mustBe "john.smith@example.com"
+
+        returnCreateRequest.totalDutyDue.totalDutyDueVapingProducts mustBe 10500
+        returnCreateRequest.totalDutyDue.totalDutySpoiltProduct mustBe 0
+        returnCreateRequest.totalDutyDue.totalDutyUnderDeclaration mustBe 0
+        returnCreateRequest.totalDutyDue.totalDutyOverDeclaration mustBe 0
+        returnCreateRequest.totalDutyDue.totalDue mustBe 10500
+      }
+
+      "successfully build a return submission for a nil return" in {
+        val userAnswers = nilReturnUserAnswers(vpdId, periodKey)
+          .set(DeclareDutyPage, false).success.value
+          .set(DeclarationPage, declaration).success.value
+
+
+        val returnCreateRequest = service.buildSubmission(userAnswers, obligation, vpdId, Map(PeriodKey(obligation.periodKey) -> 1050))
+
+        returnCreateRequest.vapingProductsProduced.vapingProdManufactured mustBe no
+        returnCreateRequest.vapingProductsProduced.returns.size mustBe 0
+
+        returnCreateRequest.spoiltProduct.get.spoiltProductFilled mustBe no
+        returnCreateRequest.underDeclaration.get.underDeclFilled mustBe no
+        returnCreateRequest.overDeclaration.get.overDeclFilled mustBe no
+
+        returnCreateRequest.otherOptions.get.vapingProductUnderDutySuspense mustBe no
+
+        returnCreateRequest.periodKey mustBe periodKey.value
+        returnCreateRequest.declaration.fullName mustBe "John Smith"
+        returnCreateRequest.declaration.capacityInWhichSigned mustBe "Director"
+        returnCreateRequest.declaration.signeesEmailAddress mustBe "john.smith@example.com"
+
+        returnCreateRequest.totalDutyDue.totalDutyDueVapingProducts mustBe 0
+        returnCreateRequest.totalDutyDue.totalDutySpoiltProduct mustBe 0
+        returnCreateRequest.totalDutyDue.totalDutyUnderDeclaration mustBe 0
+        returnCreateRequest.totalDutyDue.totalDutyOverDeclaration mustBe 0
+        returnCreateRequest.totalDutyDue.totalDue mustBe 0
+      }
+
+      "successfully build a return submission with spoilt products" in {
+        val spoiltVolumes = List(
+          SpoiltVolumeByPeriod(volume = 500, periodKey = november2026),
+          SpoiltVolumeByPeriod(volume = 300, periodKey = october2026)
+        )
+
+        val userAnswers = nilReturnUserAnswers(vpdId, december2026)
+          .set(DeclareDutyPage, true).success.value
+          .set(EnterDutyAmountPage, BigDecimal("1000")).success.value
+          .set(SpoiltVolumeByPeriodPage, spoiltVolumes).success.value
+          .set(DeclarationPage, declaration).success.value
+
+        val returnCreateRequest = service.buildSubmission(userAnswers, openObligation(december2026), vpdId, Map(october2026 -> 1050, november2026 -> 1050, december2026 -> 1050))
+
+        returnCreateRequest.vapingProductsProduced.vapingProdManufactured mustBe yes
+        returnCreateRequest.vapingProductsProduced.returns.size mustBe 1
+        returnCreateRequest.vapingProductsProduced.returns.head mustBe RegularReturn("641", 105.0, 1, 10500)
+
+        returnCreateRequest.spoiltProduct.get.spoiltProductFilled mustBe yes
+        returnCreateRequest.underDeclaration.get.underDeclFilled mustBe no
+        returnCreateRequest.overDeclaration.get.overDeclFilled mustBe no
+
+        returnCreateRequest.otherOptions.get.vapingProductUnderDutySuspense mustBe no
+
+        returnCreateRequest.periodKey mustBe december2026.value
+        returnCreateRequest.declaration.fullName mustBe "John Smith"
+        returnCreateRequest.declaration.capacityInWhichSigned mustBe "Director"
+        returnCreateRequest.declaration.signeesEmailAddress mustBe "john.smith@example.com"
+
+        returnCreateRequest.totalDutyDue.totalDutyDueVapingProducts mustBe 10500
+        returnCreateRequest.totalDutyDue.totalDutySpoiltProduct mustBe 8400
+        returnCreateRequest.totalDutyDue.totalDutyUnderDeclaration mustBe 0
+        returnCreateRequest.totalDutyDue.totalDutyOverDeclaration mustBe 0
+        returnCreateRequest.totalDutyDue.totalDue mustBe 2100
+      }
+
+      "successfully build a return submission with duty suspense movements" in {
+        val dutySuspenseVolumes = DutySuspenseVolumes(volumeReceived = 1000, volumeMoved = 500)
+
+        val userAnswers = nilReturnUserAnswers(vpdId, periodKey)
+          .set(DeclareDutyPage, true).success.value
+          .set(EnterDutyAmountPage, BigDecimal("1000")).success.value
+          .set(DeclareDutySuspensePage, true).success.value
+          .set(EnterDutySuspensePage, dutySuspenseVolumes).success.value
+          .set(DeclarationPage, declaration).success.value
+
+        val returnCreateRequest = service.buildSubmission(userAnswers, obligation, vpdId, Map(PeriodKey(obligation.periodKey) -> 1050))
+
+        returnCreateRequest.vapingProductsProduced.vapingProdManufactured mustBe yes
+        returnCreateRequest.vapingProductsProduced.returns.size mustBe 1
+        returnCreateRequest.vapingProductsProduced.returns.head mustBe RegularReturn("641", 105.0, 1, 10500)
+
+        returnCreateRequest.spoiltProduct.get.spoiltProductFilled mustBe no
+        returnCreateRequest.underDeclaration.get.underDeclFilled mustBe no
+        returnCreateRequest.overDeclaration.get.overDeclFilled mustBe no
+
+        returnCreateRequest.otherOptions.get.vapingProductUnderDutySuspense mustBe yes
+        returnCreateRequest.otherOptions.get.volumeMovedToDutySuspense mustBe Some(0.5)
+        returnCreateRequest.otherOptions.get.volumeMovedFromDutySuspense mustBe Some(1.0)
+
+        returnCreateRequest.periodKey mustBe periodKey.value
+        returnCreateRequest.declaration.fullName mustBe "John Smith"
+        returnCreateRequest.declaration.capacityInWhichSigned mustBe "Director"
+        returnCreateRequest.declaration.signeesEmailAddress mustBe "john.smith@example.com"
+
+        returnCreateRequest.totalDutyDue.totalDutyDueVapingProducts mustBe 10500
+        returnCreateRequest.totalDutyDue.totalDutySpoiltProduct mustBe 0
+        returnCreateRequest.totalDutyDue.totalDutyUnderDeclaration mustBe 0
+        returnCreateRequest.totalDutyDue.totalDutyOverDeclaration mustBe 0
+        returnCreateRequest.totalDutyDue.totalDue mustBe 10500
+      }
+
+      "fail to build a return submission when declaration details are missing" in {
+        val userAnswers = nilReturnUserAnswers(vpdId, periodKey)
+          .set(DeclareDutyPage, true).success.value
+          .set(EnterDutyAmountPage, BigDecimal("1000")).success.value
+
+        val exception = intercept[IllegalStateException] {
+          service.buildSubmission(userAnswers, obligation, vpdId, Map(PeriodKey(obligation.periodKey) -> 1050))
+        }
+
+        exception.getMessage must include("Declaration details are required")
+      }
+    }
+  }
+
+  private def nilReturnUserAnswers(vpdId: VpdId, periodKey: PeriodKey) = {
+    ReturnsUserAnswers.getEmptyReturnsUA(vpdId, periodKey)
+      .set(DeclareDutyPage          , false).success.value
+      .set(DeclareSpoiltProductsPage, false).success.value
+      .set(DeclareDutySuspensePage  , false).success.value
+  }
+}
