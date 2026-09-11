117fe1c3dde4dfc760397a55f666d295142a2cd2
ghousedev <49005870+ghousedev@users.noreply.github.com>
2026-09-11 09:16:21 +0100

    VPD-2028: Add check insolvency action to direct debit controllers

--- app/controllers/payments/StartBtaDirectDebitController.scala
+++ app/controllers/payments/StartBtaDirectDebitController.scala
@@ -18,7 +18,7 @@ package controllers.payments
 
 import config.FrontendAppConfig
 import connectors.payments.DirectDebitConnector
-import controllers.actions.ApprovedVapingManufacturerAuthAction
+import controllers.actions.{ApprovedVapingManufacturerAuthAction, CheckInsolvencyAction}
 import controllers.actions.returns.ReturnsEnabledAction
 import models.payments.StartDirectDebitRequest
 import play.api.Logging
@@ -31,13 +31,14 @@ import scala.concurrent.ExecutionContext
 class StartBtaDirectDebitController @Inject()(
                                                identify: ApprovedVapingManufacturerAuthAction,
                                                returnsEnabled: ReturnsEnabledAction,
+                                               checkInsolvencyAction: CheckInsolvencyAction,
                                                connector: DirectDebitConnector,
                                                config: FrontendAppConfig,
                                                val controllerComponents: MessagesControllerComponents
                                              )(using ExecutionContext) extends FrontendBaseController with Logging {
 
   def startDirectDebit(): Action[AnyContent] =
-    (identify andThen returnsEnabled).async { implicit request =>
+    (identify andThen checkInsolvencyAction andThen returnsEnabled).async { implicit request =>
       val urls = s"${config.continueToBta}"
       val startRequest = StartDirectDebitRequest(returnUrl = urls, backUrl = urls)
 
